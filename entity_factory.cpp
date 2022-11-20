a8c6b9a918964c8b1072ff0b6882a1e2759f5bb6
Mathias <mathiaswi177@gmail.com>
2022-11-19 19:14:05 -0800

    Function signatures for EntityControllerFactory

---
+++ entity_factory.cpp
@@ -0,0 +1,15 @@
+#include "entity_factory.h"
+
+EntityControllerFactory::EntityControllerFactory() {}
+
+EntityControllerFactory::~EntityControllerFactory() {}
+
+EntityControllerFactory* EntityControllerFactory::getInstance() {
+	/*
+		Get a pointer to the singleton instance of EntityControllerFactory
+	*/
+}
+
+EntityController* EntityControllerFactory::createFromChar(char c) {
+
+}
