75ed1a4791c21b659772023029a4802c3f6ca089
Mathias <mathiaswi177@gmail.com>
2022-12-13 18:52:43 -0800

    Header for chaser implementing A* algorithm

---
+++ a_star_chaser.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "entity_controller.h"
+#include "position.h"
+#include "game.h"
+#include <map>
+#include <vector>
+#include <string>
+
+class AStarChaser : public EntityController {
+private:
+	AStarChaser(const AStarChaser&);
+	AStarChaser& operator=(const AStarChaser&);
+
+	void pathExtension(Game *game, std::vector<Position> pastPositions, std::string path, Position curPos, const Position& heroPos, std::map<int, std::string>& pathMap, Entity* entity);
+	bool checkMove(Game* game, std::vector<Position> pastPositions, const Position& source, const Position& hypotheticalPos, Entity* entity);
+
+public:
+	AStarChaser();
+	virtual ~AStarChaser();
+	virtual Direction getMoveDirection(Game* game, Entity* entity) override;
+	virtual bool isUser() const override;
+};
